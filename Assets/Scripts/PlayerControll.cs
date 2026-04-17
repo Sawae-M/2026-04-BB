1187099a96c7690f405c27f1797ea29ea873d62e
Sawae-M <mikann22442244@gmail.com>
2026-04-17 15:47:59 +0900

    コード編集

--- Assets/Scripts/PlayerControll.cs
+++ Assets/Scripts/PlayerControll.cs
@@ -2,14 +2,34 @@ using UnityEngine;
 
 public class PlayerControll : MonoBehaviour
 {
+    [Header("�ړ��ݒ�")]
+    public float moveSpeed = 5f;
+    public float turnSpeed = 10f;
+
+    private Rigidbody rb;
+    private float moveInput;
+    private float turnInput;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+
+        transform.Rotate(Vector3.up * turnInput * turnSpeed * Time.fixedDeltaTime);
+    }
+    void ApplyMovement()
+    {
+        Vector3 movement = transform.forward * moveInput *moveSpeed;
+        rb.linearVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z);
+        //y�͏d�͂Ȃǂ��ێ����邽�߂�rb.velocity�ŕێ�
     }
 }
