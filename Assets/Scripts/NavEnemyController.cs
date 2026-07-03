9126f1cbaff89b38a14205b7d05bed76f877b6fd
Sawae-M <mikann22442244@gmail.com>
2026-07-04 01:13:08 +0900

    クリアシーンボタン設置

--- Assets/Scripts/NavEnemyController.cs
+++ Assets/Scripts/NavEnemyController.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
-
+using UnityEngine.AI;
 public class NavEnemyController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform player;
+    private NavMeshAgent navMeshAgent;
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (player != null)
+            navMeshAgent.SetDestination(player.position);
     }
 }
