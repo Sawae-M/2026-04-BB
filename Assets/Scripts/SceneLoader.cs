647569aec09263331dd5c06c15119f2296231acb
Sawae-M <mikann22442244@gmail.com>
2026-07-13 18:03:23 +0900

    AIDOGに変更

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,10 +4,20 @@ using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip clickSound;
     public string nextSceneName;
 
     public void OnClick()
     {
+        StartCoroutine(PlaySoundAndChangeScene());
+    }
+
+    private System.Collections.IEnumerator PlaySoundAndChangeScene()
+    {
+        audioSource.PlayOneShot(clickSound);
+        yield return new WaitForSeconds(clickSound.length);
+
         SceneManager.LoadScene(nextSceneName);
     }
 }
