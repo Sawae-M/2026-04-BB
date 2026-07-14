66a5e2f7fd3ee760d804fea6beb07c30edcad402
Sawae-M <mikann22442244@gmail.com>
2026-07-15 03:02:25 +0900

    ボタンの見た目変更・アイテムSE追加

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -5,11 +5,17 @@ public class Item : MonoBehaviour
     [SerializeField] private string triggeringTag = "Player";
     [SerializeField] private bool pickupDestroysItem = true;
 
+    [SerializeField] private AudioClip pickupSound;
+    [SerializeField, Range(0f, 1f)] private float pickupVolume = 1f; 
+
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag(triggeringTag)) return;
         RotSystem.Instance?.Restore();
 
+        if (pickupSound != null)
+            AudioSource.PlayClipAtPoint(pickupSound, transform.position, pickupVolume);
+ 
         if (pickupDestroysItem)
             Destroy(gameObject);
     }
