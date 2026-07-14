abebd9c9ca6feae95c84ca274227fc55ca21de4c
Sawae-M <mikann22442244@gmail.com>
2026-07-14 13:35:17 +0900

    dotweenインポート

---
+++ Assets/Scripts/RotSystem.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DGTweening;
+
+public class RotSystem : MonoBehaviour
+{
+    [SerializeField] private Renderer targetRenderer;
+    [SerializeField] private Image targetImage;
+}
