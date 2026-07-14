ccad5d9c4ed7192be308c44eaa6d2fb0b6197fe9
Sawae-M <mikann22442244@gmail.com>
2026-07-15 01:48:29 +0900

    アイテム実装

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     [SerializeField] private string triggeringTag = "Player";
+    [SerializeField] private bool pickupDestroysItem = true;
 
-    private void OnTrrigerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag(triggeringTag)) return;
         RotSystem.Instance?.Restore();
+
+        if (pickupDestroysItem)
+            Destroy(gameObject);
     }
 }
--- Assets/Scripts/RotSystem.cs
+++ Assets/Scripts/RotSystem.cs
@@ -2,26 +2,29 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
-using System.Runtime.CompilerServices;
 
 public class RotSystem : MonoBehaviour
 {
     [SerializeField] private Renderer targetRenderer;
-    [SerializeField] private Image targetImage;
+    [SerializeField] private string colorPropertyName = "_Color";
+    [SerializeField] private Slider targetSlider;
 
     [SerializeField] private string sceneToLoad = "NextScene";
-
     [SerializeField] private float drainDuration = 5f;
+
     [SerializeField, Range(0f, 1f)] private float restoreAmount = 0.3f;
     [SerializeField] private float restoreDuration = 0.5f;
 
     [SerializeField] private bool changeUIColorByFill = true;
+    [SerializeField] private Image sliderFillImage;
     [SerializeField] private Color fullColor = Color.green;
     [SerializeField] private Color emptyColor = Color.red;
 
     public static RotSystem Instance { get; private set; }
 
     private Color _origimnalObjectColor = Color.white;
+    private SpriteRenderer _targetSpriteRenderer;
+    private Material[] _targetMaterials;
     private Tween _tween;
     private bool _isSceneChanging;
     private float _progress = 1f;
@@ -31,12 +34,28 @@ public class RotSystem : MonoBehaviour
     {
         Instance = this;
 
-        if(targetRenderer != null) 
-            _origimnalObjectColor = targetRenderer.material.color;
+        if (targetRenderer != null )
+        {
+            _targetSpriteRenderer = targetRenderer as SpriteRenderer;
+            if (_targetSpriteRenderer != null)
+            {
+                _origimnalObjectColor = _targetSpriteRenderer.color;
+            }
+            else
+            {
+                _targetMaterials = targetRenderer.materials;
 
-        if (targetImage != null)
-            targetImage.type = Image.Type.Filled;
+                _origimnalObjectColor = _targetMaterials.Length > 0
+                    ? _targetMaterials[0].color
+                    : Color.white;
+            }
+        }
 
+            if (targetSlider != null)
+        {
+            targetSlider.minValue = 0f;
+            targetSlider.maxValue = 1f;
+        }
         ApplyProgress(_progress);
     }
 
@@ -62,15 +81,27 @@ public class RotSystem : MonoBehaviour
     private void ApplyProgress(float value) 
         {
             if (targetRenderer != null)
-                targetRenderer.material.color = Color.Lerp(Color.black, _origimnalObjectColor, value);
-
-            if (targetImage != null)
             {
-                targetImage.fillAmount = value;
-            if (changeUIColorByFill)
-                targetImage.color = Color.Lerp(emptyColor, fullColor, value);
+                Color newColor = Color.Lerp(Color.black, _origimnalObjectColor, value);
+                if (_targetSpriteRenderer != null)
+                {
+                    _targetSpriteRenderer.color = newColor;
+                }
+                else if (_targetMaterials != null)
+                {
+                    foreach (var mat in _targetMaterials)
+                    {
+                         mat.color = newColor;
+                    }
+                }
             }
-        }
+            if (targetSlider != null)
+            {
+                targetSlider.value = value;
+                    if (changeUIColorByFill && sliderFillImage != null)
+                        sliderFillImage.color = Color.Lerp(emptyColor, fullColor, value);
+            }
+    }
     private void OnEmpty()
     {
         if (_isSceneChanging) return;
